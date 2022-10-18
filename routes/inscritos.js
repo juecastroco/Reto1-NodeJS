e2c7f931ac16796f149f3598950ec5343cfa7d9d
juecastroco <juecastroco11@hotmail.com>
2022-10-17 19:35:25 -0500

    Inclusión de método para eliminar inscripción

--- routes/inscritos.js
+++ routes/inscritos.js
@@ -49,8 +49,13 @@ router.patch('/:id', async (req, res) => {
 });
 
 //Eliminar un Inscrito
-router.delete('/:id', async (req, res) => {
-
+router.delete('/:id', getInscrito, async (req, res) => {
+    try {
+        await res.inscrito.remove();
+        res.status(200).json({message: 'Inscripción eliminada con éxito'});
+    } catch (err) {
+        res.status(500).json({message: err.message});
+    }
 });
 
 async function getInscrito (req, res, next) {
